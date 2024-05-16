21e5f2e015570ab31c7212bffbd42440190dead9
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-05-16 16:01:49 -0500

    first commit

---
+++ src/test/java/com/ulima/curso/softwareii/freelancedev/FreelanceDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulima.curso.softwareii.freelancedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceDevApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
