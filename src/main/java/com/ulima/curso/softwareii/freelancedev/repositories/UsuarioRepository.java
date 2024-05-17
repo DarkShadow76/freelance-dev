4051bf9b24293e78613b2f644617921146b6ed9c
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-05-16 23:24:57 -0500

    entidades, rep, services, Rol y usuario

---
+++ src/main/java/com/ulima/curso/softwareii/freelancedev/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.ulima.curso.softwareii.freelancedev.repositories;
+
+import com.ulima.curso.softwareii.freelancedev.entities.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
+  boolean existsByNombre(String nombre);
+  Optional<Usuario> findByNombre(String nombre);
+}
---
+++ src/main/java/com/ulima/curso/softwareii/freelancedev/services/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.ulima.curso.softwareii.freelancedev.services;
+
+import com.ulima.curso.softwareii.freelancedev.entities.Usuario;
+
+import java.util.List;
+
+public interface UsuarioService {
+  List<Usuario> findAll();
+  Usuario save(Usuario usuario);
+  boolean existByNombre(String nombre);
+}
